30172181c6c30abde2d3cdb2f3c0dc8d693bd20f
Dante Cesa <dantec@mac.com>
2022-02-09 17:33:23 -0800

    v1

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -5,37 +5,134 @@
 //  Created by Dante Cesa on 2/8/22.
 //
 
+import CoreImage
+import CoreImage.CIFilterBuiltins
 import SwiftUI
 
 struct ContentView: View {
     @State private var image: Image?
-    @State private var inputImage: UIImage?
-    @State private var showingImagePicker: Bool = false
+    @State private var incomingImage: UIImage?
+    @State private var processedImage: UIImage?
+    
+    @State private var showingAddPhotosSheet: Bool = false
+    @State private var showingFilterSheet: Bool = false
+    
+    @State private var filterIntensity: Double = 0.0
+    @State private var currentFilter: CIFilter = CIFilter.sepiaTone()
+    let context = CIContext()
     
     var body: some View {
-        VStack {
-            image?
-                .resizable()
-                .scaledToFill()
-                .frame(width: 300, height: 300)
-            
-            Button("Select Image…") {
-                showingImagePicker = true
-            }
-            
-            Button("Save Image") {
-                guard let inputImage = inputImage else { return }
+        NavigationView {
+            VStack {
+                ZStack {
+                    if image == nil {
+                        Rectangle()
+                            .fill(.thinMaterial)
+                    } else {
+                        Rectangle()
+                            .fill(.black)
+                    }
+                    
+                    image?
+                        .resizable()
+                        .scaledToFit()
+                    
+                    if image == nil {
+                        Text("Tap to select a photo")
+                            .foregroundColor(.secondary)
+                    }
+                }
+                .padding(.vertical)
+                .onTapGesture {
+                    showingAddPhotosSheet = true
+                }
                 
-                let imageSaver = ImageSaver()
-                imageSaver.writeToPhotoAlbum(image: inputImage)
+                VStack {
+                    HStack {
+                        Text("Intensity")
+                        Slider(value: $filterIntensity , in: 0...1)
+                            .padding(.vertical)
+                    }.onChange(of: filterIntensity) { _ in
+                        applyProcessing()
+                    }
+                    
+                    HStack {
+                        Button("Change Filter") { showingFilterSheet = true }
+                        Spacer()
+                        Button(action: { saveImage() },
+                               label: { Text("Save") })
+                    }
+                    .disabled(image == nil)
+                }
+                .padding([.horizontal, .bottom])
+            }
+            .onChange(of: incomingImage) { _ in loadImage() }
+            .navigationTitle("InstaFilter")
+            .preferredColorScheme(.dark)
+            .sheet(isPresented: $showingAddPhotosSheet) { ImagePicker(image: $incomingImage) }
+            .confirmationDialog("Choose a Filter", isPresented: $showingFilterSheet) {
+                Button("Crystalize") { currentFilter = CIFilter.crystallize(); loadImage() }
+                Button("Edges") { currentFilter = CIFilter.edges(); loadImage() }
+                Button("Gaussian Blur") { currentFilter = CIFilter.gaussianBlur(); loadImage() }
+                Button("Pixellate") { currentFilter = CIFilter.pixellate(); loadImage() }
+                Button("Sepia Tone") { currentFilter = CIFilter.sepiaTone(); loadImage() }
+                Button("Unsharp Mask") { currentFilter = CIFilter.unsharpMask(); loadImage() }
+                Button("Vignette") { currentFilter = CIFilter.vignette(); loadImage() }
+                Button("Cancel", role: .cancel) { }
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    if image != nil { Button(action: { showingAddPhotosSheet = true }, label: { Text("New Image") }) }
+                }
             }
         }
-        .sheet(isPresented: $showingImagePicker) {
-            ImagePicker(image: $inputImage)
+    }
+    
+    func loadImage() {
+        guard let incomingImage = incomingImage else { return }
+        
+        let beginImage = CIImage(image: incomingImage)
+        currentFilter.setValue(beginImage, forKey: kCIInputImageKey)
+        applyProcessing()
+    }
+    
+    func saveImage() {
+        guard let processedImage = processedImage else { return }
+        
+        let imageSaver = ImageSaver()
+        
+        imageSaver.successHandler = {
+            print("Image saved successfully")
         }
-        .onChange(of: inputImage) { _ in
-            guard let inputImage = inputImage else { return }
-            image = Image(uiImage: inputImage)
+        
+        imageSaver.errorHandler = {
+            print("Something went wrong saving the image: \($0.localizedDescription)")
+        }
+        
+        imageSaver.writeToPhotoAlbum(image: processedImage)
+    }
+    
+    func applyProcessing() {
+        let inputKeys = currentFilter.inputKeys
+        
+        if inputKeys.contains(kCIInputIntensityKey) {
+            currentFilter.setValue(filterIntensity, forKey: kCIInputIntensityKey)
+        }
+        
+        if inputKeys.contains(kCIInputRadiusKey) {
+            currentFilter.setValue(filterIntensity * 200, forKey: kCIInputRadiusKey)
+        }
+        
+        if inputKeys.contains(kCIInputScaleKey) {
+            currentFilter.setValue(filterIntensity * 10, forKey: kCIInputScaleKey)
+        }
+        
+        guard let outputImage = currentFilter.outputImage else { return }
+        
+        if let cgImage = context.createCGImage(outputImage, from: outputImage.extent) {
+            let uiImage = UIImage(cgImage: cgImage)
+            image = Image(uiImage: uiImage)
+            processedImage = uiImage
         }
     }
 }
